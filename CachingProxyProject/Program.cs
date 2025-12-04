3015fab895c13c0d6a05e74428723db6f9573de9
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-12-03 22:45:45 -0400

    Initial commit

---
+++ CachingProxyProject/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CachingProxyProject;
+using Cocona;
+
+CoconaLiteApp.Run(([Option('p')] int port, [Option('o')] string origin) =>
+{
+    
+});
