f25cac7f230e6f1f7bd008c2d7cebaefaa2b90f4
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-12-04 02:13:04 -0400

    Update Program.cs

--- CachingProxyProject/Program.cs
+++ CachingProxyProject/Program.cs
@@ -1,9 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net;
 using CachingProxyProject;
 using Cocona;
 
-CoconaLiteApp.Run(([Option('p')] int port, [Option('o')] string origin) =>
+
+CoconaLiteApp.Run(async ([Option('p')] int port, [Option('o')] string origin) =>
 {
-    
+    Console.WriteLine($"Starting server at port {port}...\nOrigin address: {origin}\n\n");
+
+    var proxyServer = new ProxyServer(port, origin);
+    proxyServer.Start();
+    Console.CancelKeyPress += (sender, eventArgs) => { proxyServer.Stop(); };
+
+    while (true)
+    {
+        try
+        {
+            await proxyServer.Receive();
+        }
+        catch (HttpListenerException)
+        {
+            break;
+        }
+    }
+
+    Console.WriteLine("Server stopped");
 });
