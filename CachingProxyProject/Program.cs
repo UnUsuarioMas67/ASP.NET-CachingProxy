e864238a938c6892e3a801f8dad45f45cde76b5f
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-12-10 02:51:07 -0400

    Add CachingProxyUI.cs

--- CachingProxyProject/Program.cs
+++ CachingProxyProject/Program.cs
@@ -1,36 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Net;
 using CachingProxyProject.Caching;
-using CachingProxyProject.Server;
-using Cocona;
+using CachingProxyProject.UI;
 using Microsoft.Extensions.Configuration;
 
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.Development.json")
     .Build();
 
-CoconaLiteApp.Run(async ([Option('p')] int port, [Option('o')] string origin) =>
-{
-    Console.WriteLine($"Starting server at port {port}...\nOrigin address: {origin}\n\n");
+var redis = new RedisConnection(config);
 
-    var redis = new RedisConnection(config);
-    var proxyServer = new ProxyServer(port, origin, redis);
-    proxyServer.Start();
-    
-    Console.CancelKeyPress += (sender, eventArgs) => { proxyServer.Stop(); };
-
-    while (true)
-    {
-        try
-        {
-            await proxyServer.Receive();
-        }
-        catch (HttpListenerException)
-        {
-            break;
-        }
-    }
-
-    Console.WriteLine("Server stopped");
-});
+var ui = new CachingProxyUI(redis);
+ui.Run();
---
+++ CachingProxyProject/UI/CachingProxyUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using CachingProxyProject.Caching;
+using CachingProxyProject.Server;
+using Cocona;
+using Microsoft.Extensions.Configuration;
+
+namespace CachingProxyProject.UI;
+
+public class CachingProxyUI(RedisConnection redis)
+{
+    public void Run()
+    {
+        CoconaLiteApp.Run(async ([Option('p')] int port, [Option('o')] string origin) =>
+        {
+            Console.WriteLine($"Starting server at port {port}...\nOrigin address: {origin}\n\n");
+
+            var proxyServer = new ProxyServer(port, origin, redis);
+            proxyServer.Start();
+    
+            Console.CancelKeyPress += (sender, eventArgs) => { proxyServer.Stop(); };
+
+            while (true)
+            {
+                try
+                {
+                    await proxyServer.Receive();
+                }
+                catch (HttpListenerException)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Server stopped");
+        });
+    }
+}
