73b8c2b13ce91d86a78d736d5c9bd21b5cbfc47f
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-12-12 00:48:30 -0400

    New keys stored in redis will now include a prefix
    When saving to the redis databse the prefix 'proxy:' will be added before the given key.

--- CachingProxyProject/Caching/RedisConnection.cs
+++ CachingProxyProject/Caching/RedisConnection.cs
@@ -6,8 +6,10 @@ namespace CachingProxyProject.Caching;
 
 public class RedisConnection
 {
+    private const string Prefix = "proxy:";
+    
     private readonly IConfiguration _config;
-    private readonly IDatabase _db;
+    private readonly ConnectionMultiplexer _muxer;
 
     public RedisConnection(IConfiguration config)
     {
@@ -17,25 +19,26 @@ public class RedisConnection
         var user = _config["UserName"]!;
         var password = _config["Password"];
         
-        var muxer = ConnectionMultiplexer.Connect(new ConfigurationOptions
+        _muxer = ConnectionMultiplexer.Connect(new ConfigurationOptions
         {
             EndPoints = { endpoint },
             User = user,
             Password = password
         });
-
-        _db = muxer.GetDatabase();
     }
 
     public async Task<bool> SaveAsJson(string key, object obj)
     {
-        var expireMinutes = _config.GetValue<int>("ExpireMinutes");
+        var db = _muxer.GetDatabase();
         
-        var success = await _db.JSON().SetAsync(key, "$", obj);
-        await _db.KeyExpireAsync(key, DateTime.UtcNow.AddMinutes(expireMinutes));
+        var fullKey = $"{Prefix}{key}";
+        var success = await db.JSON().SetAsync(fullKey, "$", obj);
+        
+        var expireMinutes = _config.GetValue<int>("ExpireMinutes");
+        await db.KeyExpireAsync(fullKey, DateTime.UtcNow.AddMinutes(expireMinutes));
         return success;
     }
 
     public async Task<T?> GetJson<T>(string key)
-        => await _db.JSON().GetAsync<T>(key);
+        => await _muxer.GetDatabase().JSON().GetAsync<T>(Prefix + key);
 }
